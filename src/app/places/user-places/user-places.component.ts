54cb06927b5c5465772899022f8a856a57c2daec
Abdul_Malik <malik.info28@gmail.com>
2025-04-19 03:58:13 +0530

    error fixed and updates the UI immediatley when the place is selected

--- src/app/places/user-places/user-places.component.ts
+++ src/app/places/user-places/user-places.component.ts
@@ -13,20 +13,16 @@ import { PlacesService } from '../places.service';
   imports: [PlacesContainerComponent, PlacesComponent],
 })
 export class UserPlacesComponent implements OnInit {
-  places = signal<Place[] | undefined>(undefined);
   isFetching = signal(false);
   error = signal('');
-
+  
   private placesService = inject(PlacesService);
   private destroyRef = inject(DestroyRef);
+  places = this.placesService.loadedUserPlaces;
 
   ngOnInit() {
     this.isFetching.set(true);
     const subscription = this.placesService.loadUserPlaces().subscribe({
-      next: (places) => {
-        // console.log(resData.places);
-        this.places.set(places);
-      },
       error: (err: Error) => {
         // console.log(err);
         this.error.set(err.message);
